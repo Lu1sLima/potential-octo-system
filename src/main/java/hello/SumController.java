f5427a6d4f9aab3a164acae205d43ee8f2a5ca27
Luís Lima <llima@cipucrs.pucrs.br>
2019-10-10 18:34:17 -0300

    #html

---
+++ src/main/java/hello/SumController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SumController {
+
+    @GetMapping("/sum")
+    public String greeting(@RequestParam(name="a") int a,@RequestParam(name="b")int b, Model model) {
+	model.addAttribute("result", a+b);
+        return "sum";
+    }
+
+}
